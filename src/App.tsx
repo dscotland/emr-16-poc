45fdc045954fc8f9ab4e3607b028d55ac30861a5
Doneil <dscotland@qualityworkscg.com>
2023-09-16 22:26:27 -0500

    Initial commit react-picture-annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { ReactPictureAnnotation } from 'react-picture-annotation';
+import { Box } from '@chakra-ui/react'
 
 
 function App() {
 
-  return (
-    <div className="App">
-    </div>
-  );
+	const [pageSize, setPageSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight
+	});
+
+	const onResize = () => {
+		setPageSize({ width: window.innerWidth, height: window.innerHeight });
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', onResize);
+		return () => window.removeEventListener('resize', onResize);
+	}, []);
+
+	return (
+		<div className="App">
+			<ReactPictureAnnotation
+				image="https://source.unsplash.com/random/"
+				onSelect={(selectedId)=>{console.log(selectedId)}}
+				onChange={(data)=>{console.log(data)}}
+				width={600}
+				height={600}
+			/>
+
+		</div>
+	);
 }
 
 export default App;
