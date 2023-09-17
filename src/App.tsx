86915cde318be7ecc310790941449692eff4e36d
Doneil <dscotland@qualityworkscg.com>
2023-09-16 21:56:32 -0500

    Initial main commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React, { useEffect, useRef } from 'react';
 import './App.css';
-import PdfViewerComponent from './components/PdfViewerComponent';
-
-function DocumentViewerComponent() {
-	return (
-		<div className="PDF-viewer">
-			<PdfViewerComponent
-				document={"document.pdf"}
-			/>
-		</div>
-	);
-}
 
 
 function App() {
 
   return (
     <div className="App">
-      {DocumentViewerComponent()}
     </div>
   );
 }
